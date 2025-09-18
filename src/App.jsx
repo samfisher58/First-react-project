bc3d388acb09632d802f68588aafa27fb4c84ae0
samfisher58 <symonislam58@gmail.com>
2025-09-18 20:43:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Countries from './countries/component/countries'
+
+function App() {
+ 
+
+  return (
+    <>     
+    <Countries></Countries>
+      <h1> React world on the go...</h1>      
+    </>
+  )
+}
+
+export default App
